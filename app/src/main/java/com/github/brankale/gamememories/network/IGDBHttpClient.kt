d5f7021150634bd406ff57b073fcf48005a0f536
brankale <45881310+Brankale@users.noreply.github.com>
2024-08-04 16:24:49 +0200

    IGDB http client: - added json responses support - added default headers - added url configurations - removed test code

--- app/src/main/java/com/github/brankale/gamememories/network/IGDBHttpClient.kt
+++ app/src/main/java/com/github/brankale/gamememories/network/IGDBHttpClient.kt
@@ -4,19 +4,21 @@ import com.github.brankale.gamememories.BuildConfig
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.auth.Auth
 import io.ktor.client.plugins.auth.providers.BearerTokens
 import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.header
 import io.ktor.client.request.post
-import io.ktor.client.statement.bodyAsText
+import io.ktor.http.URLProtocol
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
 private val IGDB_HOST = "api.igdb.com"
-private val BASE_URL = "https://$IGDB_HOST/v4"
 
 @Serializable
 private data class TwitchAuthResponse(
@@ -25,33 +27,41 @@ private data class TwitchAuthResponse(
     @SerialName("token_type") val tokenType: String,
 )
 
-suspend fun main() {
-    val client = HttpClient(CIO) {
+val igdbHttpClient =
+    HttpClient(CIO) {
 //        install(Logging)
+        install(DefaultRequest) {
+            url {
+                protocol = URLProtocol.HTTPS
+                host = IGDB_HOST
+            }
+            header("Client-ID", BuildConfig.CLIENT_ID)
+        }
         install(Auth) {
             bearer {
                 loadTokens {
-                    val response = refreshToken()
+                    val response = refreshIGDBToken()
                     println("load tokens")
                     BearerTokens(response.accessToken, "")
                 }
                 refreshTokens {
-                    val response = refreshToken()
+                    val response = refreshIGDBToken()
                     println("refresh tokens")
                     BearerTokens(response.accessToken, "")
                 }
             }
         }
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
     }
 
-    val response = client.post("$BASE_URL/games") {
-        header("Client-ID", BuildConfig.CLIENT_ID)
-    }
-
-    println(response.bodyAsText())
-}
-
-private suspend fun refreshToken(): TwitchAuthResponse {
+private suspend fun refreshIGDBToken(): TwitchAuthResponse {
     val client = HttpClient(CIO) {
         install(ContentNegotiation) {
             json()
